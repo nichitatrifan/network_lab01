71a1110eb4eb50184d617567d346ef7499892752
nichitatrifan <nikiton254@gmail.com>
2022-01-24 12:39:24 -0600

    setup http response

--- server.py
+++ server.py
@@ -7,12 +7,27 @@ def handle_client(client_socket, addr):
     request = client_socket.recv(1024).decode('utf-8')
     headers = request.split('\n')
     get_resource = headers[0].split(' ')[1]
-    resource_path = os.path.abspath(os.getcwd()).replace('\\','/') + get_resource 
+    resource_path = os.path.abspath(os.getcwd()).replace('\\','/') + get_resource
+
     print(headers[0])
-    print('trying to get: ' + str(get_resource))
     print('resource path: ' + str(resource_path))
+
+    response = 'HTTP/1.1 200 OK\r\n' +\
+        'Date:\r\n' +\
+        'Server: localhost\r\n' +\
+        'Content-Length: 1024\r\n' +\
+        'Connection: Closed\r\n' +\
+        'Content-Type: text/html\r\n'+\
+        '\r\n' # need an empty line in between headers and data
     
-    response = 'Accepted'
+    try:
+        with open(resource_path, 'r') as resource:
+            resource_text = resource.read()
+            response += resource_text
+    except FileNotFoundError as fne:
+        print('Resource Not Found!')
+        response += 'None'
+
     client_socket.sendall(response.encode('utf-8'))
     
 
